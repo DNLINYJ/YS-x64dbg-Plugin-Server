8d5d914d3462b9e1376aae1ea8218906e4f65aa2
DNLINYJ <linqian9687654321@gmail.com>
2021-07-07 13:43:17 +0800

    更改服务器接收信息方式为GET

--- http_server.py
+++ http_server.py
@@ -2,15 +2,13 @@ from flask import Flask,request
 import json
 app = Flask(__name__)
  
-@app.route('/jmp_address',methods=['POST'])
+@app.route('/jmp_address',methods=['GET'])
 def test():
-    # {"offset":jmp指令的偏移量, "jmp_offset":jmp指令跳转的地址偏移量}
-    temp_json = json.loads(request.data.decode('utf-8'))
-    offset = temp_json['offset']
-    jmp_offset = temp_json['jmp_offset']
+    offset = request.args.get("offset")
+    jmp_offset = request.args.get("jmp_offset")
     with open("offset_jmp_already.json","r",encoding="utf-8") as f:
         json_old = json.load(f) 
-    json_old[offset] = jmp_offset
+    json_old.append({offset : jmp_offset})
     with open("offset_jmp_already.json","w",encoding="utf-8") as f:
         json.dump(json_old, f)
     return "OK"
