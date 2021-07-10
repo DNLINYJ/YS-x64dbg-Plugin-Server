ecd0b067dfdf664a764b53f60b5566145b7e9680
DNLINYJ <linqian9687654321@gmail.com>
2021-07-10 20:27:15 +0800

    最终更改

--- http_server.py
+++ http_server.py
@@ -2,28 +2,62 @@ from flask import Flask,request
 import json
 app = Flask(__name__)
 
+import difflib
+#判断相似度的方法，用到了difflib库
+def get_equal_rate_1(str1, str2):
+   return difflib.SequenceMatcher(None, str1, str2).quick_ratio()
+
+
+num = 1
+
 def ee():
-    with open("offset_jmp_already.json","w",encoding="utf-8") as f:
-        f.write("[]")
- 
+    with open("jmp.json","w",encoding="utf-8") as f:
+        json.dump({}, f)
+
+def mw():
+    with open("mov.json","w",encoding="utf-8") as f:
+        json.dump({}, f)
+
 ee()
+mw()
 
 @app.route('/jmp_address',methods=['GET'])
 def test():
     offset = request.args.get("offset")
-    jmp_offset = request.args.get("jmp_offset")
-    with open("offset_jmp_already.json","r",encoding="utf-8") as f:
-        json_old = json.load(f) 
-    json_old.append({offset : jmp_offset})
-    with open("offset_jmp_already.json","w",encoding="utf-8") as f:
-        json.dump(json_old, f)
+    jmp_offset = request.args.get("jmp_offset").replace("%20"," ")
+    with open("jmp.json","r",encoding="utf-8") as f:
+        a = json.load(f)
+    if offset not in list(a.keys()):
+        a[offset] = [jmp_offset]
+    else:
+        a[offset].append(jmp_offset)
+    with open("jmp.json","w",encoding="utf-8") as f:
+        json.dump(a, f)
     return "OK"
 
-@app.route('/jmp_offset_file_path',methods=['GET'])
-def file_path():
-    ee()
-    default_path = "D:\\jmp_files\\offset_jmp_2.json"
-    return default_path
-    
+@app.route('/mov_address',methods=['GET'])
+def mov_test():
+    offset = request.args.get("offset")
+    mov_offset = request.args.get("mov_offset").replace("%20"," ")
+    with open("mov.json","r",encoding="utf-8") as f:
+        a = json.load(f)
+    if offset not in list(a.keys()):
+        a[offset] = [mov_offset]
+    else:
+        a[offset].append(mov_offset)
+    with open("mov.json","w",encoding="utf-8") as f:
+        json.dump(a, f)
+    return "OK"
+
+@app.route('/check_jmp_command',methods=['GET'])
+def diif():
+    c = request.args.get("c")
+    return str(get_equal_rate_1(str(c), "jmp rax"))
+
+@app.route('/check_mov_command',methods=['GET'])
+def diif_mov():
+    c = request.args.get("c")
+    return str(get_equal_rate_1(str(c), "mov rax, [rcx+rax*8]"))
+
 if __name__ == '__main__':
     app.run(port=50000)
